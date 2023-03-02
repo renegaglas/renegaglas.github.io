c5bce5cff2f68c233a75d053268f60652ffd3db8
renegaglas <94482674+renegaglas@users.noreply.github.com>
2023-03-02 04:11:29 +0100

    button nav fix

--- assets/javascript/responsive.js
+++ assets/javascript/responsive.js
@@ -4,9 +4,18 @@ function responsive_smallscreen(slf)
     //navbar.ul(display = block)
     let respnav = document.getElementById("respnav");
     console.log(respnav);
-    respnav.style.display = "block";
-
-    
+    console.log(respnav.style.display);
+    console.log(respnav);
     let menu_button = document.getElementsByClassName("menu_button")[0];
-    menu_button.style.display = "none";
+    if (respnav.style.visibility === "hidden" || respnav.style.visibility === "")
+    {
+        respnav.style.display = "block";
+        respnav.style.visibility = "visible";
+    }
+    else
+    {
+        respnav.style.visibility = "hidden";
+        respnav.style.display = "flex";
+
+    }
 }
